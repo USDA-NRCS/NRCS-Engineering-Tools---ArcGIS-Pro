a71512451b9eb0c4b5c1751dd3dc20976257e25c
dylan-harwell <12001003312449@FEDIDCARD.GOV>
2024-07-07 14:51:22 -0700

    Second pass cleanup, text file loggin

--- SUPPORT/Change_Point_Coordinates.py
+++ SUPPORT/Change_Point_Coordinates.py
@@ -42,8 +42,8 @@ text = GetParameter(5)
 
 try:
     # Check out 3D and SA licenses
-    if CheckExtension('3d') == 'Available':
-        CheckOutExtension('3d')
+    if CheckExtension('3D') == 'Available':
+        CheckOutExtension('3D')
     else:
         AddMsgAndPrint('\n3D analyst extension is not enabled. Please enable 3D Analyst from the Tools/Extensions menu. Exiting...\n', 2)
         exit()
@@ -66,7 +66,7 @@ try:
     env.outputCoordinateSystem = outCS
 
     # Permanent Datasets
-    outPoints = path.join(userWorkspace, f"{projectName}_XYZ_new_coordinates.shp")
+    outPoints = path.join(userWorkspace, f"{projectName}_XYZ_new_coordinates.shp") #NOTE: should these be shapefiles??
     outTxt = path.join(userWorkspace, f"{projectName}_XYZ_new_coordinates.txt")
     # Must Have a unique name for output -- Append a unique digit to output if required
     x = 1
@@ -84,7 +84,7 @@ try:
     pointsProj = path.join(watershedGDB_path, 'pointsProj')
 
     # Check DEM Coordinate System and Linear Units
-    AddMsgAndPrint(f"\nGathering information about DEM: {path.basename(inputDEM)}\n")
+    AddMsgAndPrint(f"\nGathering information about DEM: {path.basename(inputDEM)}\n", textFilePath=textFilePath)
     desc = Describe(inputDEM)
     sr = desc.SpatialReference
     units = sr.LinearUnitName
@@ -110,19 +110,19 @@ try:
         # zUnits must be feet; no more choices       
         else:
             Zfactor = 1                 
-        AddMsgAndPrint(f"\tProjection Name: {sr.Name}")
-        AddMsgAndPrint(f"\tXY Linear Units: {units}")
-        AddMsgAndPrint(f"\tElevation Values (Z): {zUnits}") 
-        AddMsgAndPrint(f"\tCell Size: {str(desc.MeanCellWidth)} x {str(desc.MeanCellHeight)} {units}")
+        AddMsgAndPrint(f"\tProjection Name: {sr.Name}", textFilePath=textFilePath)
+        AddMsgAndPrint(f"\tXY Linear Units: {units}", textFilePath=textFilePath)
+        AddMsgAndPrint(f"\tElevation Values (Z): {zUnits}", textFilePath=textFilePath) 
+        AddMsgAndPrint(f"\tCell Size: {str(desc.MeanCellWidth)} x {str(desc.MeanCellHeight)} {units}", textFilePath=textFilePath)
     else:
-        AddMsgAndPrint(f"\n\n\t{path.basename(inputDEM)} is NOT in a projected Coordinate System. Exiting...", 2)
+        AddMsgAndPrint(f"\n\n\t{path.basename(inputDEM)} is NOT in a projected Coordinate System. Exiting...", 2, textFilePath)
         exit()
                       
     # Create Watershed FGDB and feature dataset if it doesn't exist
     if not Exists(watershedGDB_path):
         CreateFileGDB(userWorkspace, watershedGDB_name)
         CreateFeatureDataset(watershedGDB_path, 'Layers', sr)
-        AddMsgAndPrint(f"\nSuccessfully created File Geodatabase: {watershedGDB_name}")
+        AddMsgAndPrint(f"\nSuccessfully created File Geodatabase: {watershedGDB_name}", textFilePath=textFilePath)
 
     # if GDB already existed but feature dataset doesn't
     if not Exists(watershedFD):
@@ -139,7 +139,7 @@ try:
     # Update Elevation values in feet
     AddSurfaceInformation(pointsProj, inputDEM, 'Z', '', '', Zfactor)
     expression = 'round(!Z!,1)'
-    CalculateField(pointsProj, 'POINT_Z', expression, 'PYTHON_9.3')
+    CalculateField(pointsProj, 'POINT_Z', expression, 'PYTHON3')
 
     # Clean up extra fields
     DeleteField(pointsProj, 'POINT_M')
@@ -150,8 +150,8 @@ try:
 
     # Create Txt file if selected and write attributes of station points
     if text == True:
-        AddMsgAndPrint('Creating Output text file:\n')
-        AddMsgAndPrint(f"\t{str(outTxt)}\n")
+        AddMsgAndPrint('Creating Output text file:\n', textFilePath=textFilePath)
+        AddMsgAndPrint(f"\t{str(outTxt)}\n", textFilePath=textFilePath)
 
         with open(outTxt, 'w') as t:
             t.write('ID, STATION, X, Y, Z')
@@ -173,23 +173,26 @@ try:
     env.outputCoordinateSystem = tempCoordSys
 
     # Add layer to map
-    AddMsgAndPrint('Adding layer to map\n')
+    AddMsgAndPrint('Adding layer to map\n', textFilePath=textFilePath)
     SetParameterAsText(6, outPoints)
 
     # Delete Temp Layers 
-    AddMsgAndPrint('Deleting temporary files...\n')
+    AddMsgAndPrint('Deleting temporary files...\n', textFilePath=textFilePath)
     deleteScratchLayers([pointsProj])
 
     try:
         Compact(watershedGDB_path)
-        AddMsgAndPrint(f"\nSuccessfully Compacted FGDB: {path.basename(watershedGDB_path)}")    
+        AddMsgAndPrint(f"\nSuccessfully Compacted FGDB: {path.basename(watershedGDB_path)}", textFilePath=textFilePath)    
     except:
         pass
 
-    AddMsgAndPrint('Processing Complete!\n')
+    AddMsgAndPrint('Processing Complete!\n', textFilePath=textFilePath)
 
 except SystemExit:
     pass
 
 except:
-    errorMsg('Change Point Coordinates')            
+    try:
+        AddMsgAndPrint(errorMsg('Change Point Coordinates'), 2, textFilePath)
+    except:
+        AddMsgAndPrint(errorMsg('Change Point Coordinates'), 2) 
