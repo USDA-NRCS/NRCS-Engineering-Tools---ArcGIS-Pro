09a35c0c0a53b66aa9acd982ce3fa0682c41c4a6
dylan-harwell <12001003312449@FEDIDCARD.GOV>
2025-01-24 12:08:29 -0800

    Update AOI.lyrx, delete old files, remove GEODESIC from area calc

--- SUPPORT/Create_AOI.py
+++ SUPPORT/Create_AOI.py
@@ -80,8 +80,8 @@ try:
     ### Update Acres Fields ###
     SetProgressorLabel('Updating acres fields...')
     AddMsgAndPrint('\nUpdating acres fields...', textFilePath=log_file_path)
-    CalculateGeometryAttributes(output_aoi_path, 'acres_us AREA_GEODESIC', '', 'ACRES_US', project_sr, 'SAME_AS_INPUT')
-    CalculateGeometryAttributes(output_aoi_path, 'acres_intl AREA_GEODESIC', '', 'ACRES', project_sr, 'SAME_AS_INPUT')
+    CalculateGeometryAttributes(output_aoi_path, 'acres_us AREA', '', 'ACRES_US', project_sr, 'SAME_AS_INPUT')
+    CalculateGeometryAttributes(output_aoi_path, 'acres_intl AREA', '', 'ACRES', project_sr, 'SAME_AS_INPUT')
     CalculateField(output_aoi_path, 'acres_us', 'Round($feature.acres_us,2)', 'ARCADE')
     CalculateField(output_aoi_path, 'acres_intl', 'Round($feature.acres_intl,2)', 'ARCADE')
 
