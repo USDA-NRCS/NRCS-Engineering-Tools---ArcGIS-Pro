000a5092d2a2a59c530e6dbaed0853cd503f1c79
dylan-harwell <12001003312449@FEDIDCARD.GOV>
2025-02-05 15:51:44 -0800

    WIP: Create Contours tool

--- SUPPORT/Create_Contours.py
+++ SUPPORT/Create_Contours.py
@@ -7,7 +7,7 @@ from arcpy import CheckExtension, CheckOutExtension, Describe, env, GetInstallIn
     GetParameterAsText, SetParameterAsText, SetProgressorLabel
 from arcpy.management import Clip, Compact, CopyRaster, Delete, MosaicToNewRaster, Project, ProjectRaster
 from arcpy.mp import ArcGISProject
-from arcpy.sa import Contour, FocalStatistics
+from arcpy.sa import Contour
 
 from utils import AddMsgAndPrint, emptyScratchGDB, errorMsg, removeMapLayers
 
@@ -46,65 +46,65 @@ env.pyramid = 'PYRAMIDS -1 BILINEAR DEFAULT 75 NO_SKIP'
 
 ### Input Parameters ###
 project_dem = GetParameterAsText(0)
-contour_interval = GetParameter(1)
+contour_interval = GetParameterAsText(1)
 
 ### Locate Project GDB ###
 project_dem_path = Describe(project_dem).CatalogPath
-if project_dem_path.find('.gdb') > 0 and 'DEM' in project_dem_path:
+if 'EngPro.gdb' in project_dem_path and 'DEM' in project_dem_path:
     project_gdb = project_dem_path[:project_dem_path.find('.gdb')+4]
 else:
-    AddMsgAndPrint('\nSelected DEM layer is not from an Engineering project workspace. Exiting...', 2)
+    AddMsgAndPrint('\nThe selected DEM is not from an Engineering Tools project or is not compatible with this version of the toolbox. Exiting...', 2)
     exit()
 
 ### Set Paths and Variables ###
 project_workspace = path.dirname(project_gdb)
 project_name = path.basename(project_workspace)
 log_file_path = path.join(project_workspace, f"{project_name}_log.txt")
-contour_name = f"{project_name}_Contour_{str(contour_interval)}"
-contour_path = path.join(project_gdb, contour_name)
+smoothed_dem_path = path.join(project_gdb, f"{project_name}_Smooth_3_3")
+contour_name = f"{project_name}_Contour_{contour_interval}"
+contour_path = path.join(project_gdb, 'Layers', contour_name)
+# z_factor = 0.3048 # Meters to Intl Feet
 
 try:
-    logBasicSettings(log_file_path, project_workspace, project_dem, str(contour_interval))
+    removeMapLayers(map, [contour_name])
+    logBasicSettings(log_file_path, project_workspace, project_dem, contour_interval)
 
-    # Run Focal Statistics on the DEM_aoi for the purpose of generating smooth contours
-    outFocalStats = FocalStatistics(DEM_aoi, "RECTANGLE 3 3 CELL","MEAN","DATA")
-    outFocalStats.save(DEMsmooth)
-    AddMsgAndPrint("\nSuccessully Smoothed " + path.basename(DEM_aoi),0)
+    ### Create Contours ###
+    SetProgressorLabel('Creating Contours...')
+    AddMsgAndPrint('\nCreating Contours...', log_file_path=log_file_path)
+    Contour(smoothed_dem_path, contour_path, contour_interval)
 
-    Contour(DEMsmooth, ContoursTemp, interval, "0", Zfactor)
-    AddMsgAndPrint("\nSuccessfully Created " + str(interval) + " foot Contours using a Z-factor of " + str(Zfactor),0)
+    # arcpy.AddField_management(ContoursTemp, "Index", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+    # if Exists("ContourLYR"):
+    #     try:
+    #         arcpy.Delete_management("ContourLYR")
+    #     except:
+    #         pass
 
-    arcpy.AddField_management(ContoursTemp, "Index", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
-    if Exists("ContourLYR"):
-        try:
-            arcpy.Delete_management("ContourLYR")
-        except:
-            pass
+    # arcpy.MakeFeatureLayer_management(ContoursTemp,"ContourLYR","","","")
 
-    arcpy.MakeFeatureLayer_management(ContoursTemp,"ContourLYR","","","")
+    # # Every 5th contour will be indexed to 1
+    # expression = "MOD( \"CONTOUR\"," + str(float(interval) * 5) + ") = 0"
+    # arcpy.SelectLayerByAttribute_management("ContourLYR", "NEW_SELECTION", expression)
+    # del expression
 
-    # Every 5th contour will be indexed to 1
-    expression = "MOD( \"CONTOUR\"," + str(float(interval) * 5) + ") = 0"
-    arcpy.SelectLayerByAttribute_management("ContourLYR", "NEW_SELECTION", expression)
-    del expression
+    # indexValue = 1
+    # #arcpy.CalculateField_management("ContourLYR", "Index", indexValue, "VB","")
+    # arcpy.CalculateField_management("ContourLYR", "Index", indexValue, "PYTHON_9.3")
+    # del indexValue
 
-    indexValue = 1
-    #arcpy.CalculateField_management("ContourLYR", "Index", indexValue, "VB","")
-    arcpy.CalculateField_management("ContourLYR", "Index", indexValue, "PYTHON_9.3")
-    del indexValue
+    # # All othe contours will be indexed to 0
+    # arcpy.SelectLayerByAttribute_management("ContourLYR", "SWITCH_SELECTION")
+    # indexValue = 0
+    # #arcpy.CalculateField_management("ContourLYR", "Index", indexValue, "VB","")
+    # arcpy.CalculateField_management("ContourLYR", "Index", indexValue, "PYTHON_9.3")
+    # del indexValue
 
-    # All othe contours will be indexed to 0
-    arcpy.SelectLayerByAttribute_management("ContourLYR", "SWITCH_SELECTION")
-    indexValue = 0
-    #arcpy.CalculateField_management("ContourLYR", "Index", indexValue, "VB","")
-    arcpy.CalculateField_management("ContourLYR", "Index", indexValue, "PYTHON_9.3")
-    del indexValue
+    # # Clear selection and write all contours to a new feature class
+    # arcpy.SelectLayerByAttribute_management("ContourLYR","CLEAR_SELECTION")
+    # arcpy.CopyFeatures_management("ContourLYR", Contours)
 
-    # Clear selection and write all contours to a new feature class
-    arcpy.SelectLayerByAttribute_management("ContourLYR","CLEAR_SELECTION")
-    arcpy.CopyFeatures_management("ContourLYR", Contours)
-
-    AddMsgAndPrint('\nCreate DEM completed successfully', log_file_path=log_file_path)
+    AddMsgAndPrint('\nCreate Contours completed successfully', log_file_path=log_file_path)
 
 except SystemExit:
     pass
