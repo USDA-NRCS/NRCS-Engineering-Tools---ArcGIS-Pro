10a63a463b87e939003a022db51ccb4af8a5de16
dylan-harwell <12001003312449@FEDIDCARD.GOV>
2025-03-06 14:33:29 -0800

    Clarifications and updates to Stream Network tool

--- SUPPORT/Create_Stream_Network.py
+++ SUPPORT/Create_Stream_Network.py
@@ -6,7 +6,7 @@ from time import ctime
 from arcpy import CheckExtension, CheckOutExtension, Describe, env, Exists, GetInstallInfo, GetParameterAsText, \
     SetParameterAsText, SetProgressorLabel
 from arcpy.analysis import Buffer, Clip
-from arcpy.management import AddField, CalculateField, CalculateStatistics, Compact, Delete, GetCount, MosaicToNewRaster
+from arcpy.management import AddField, CalculateField, CalculateStatistics, Compact, GetCount, MosaicToNewRaster
 from arcpy.mp import ArcGISProject
 from arcpy.sa import Con, Fill, FlowAccumulation, FlowDirection, StreamLink, StreamToFeature, ZonalStatistics
 
@@ -23,7 +23,7 @@ def logBasicSettings(log_file_path, project_aoi, input_culverts, stream_threshol
         f.write('User Parameters:\n')
         f.write(f"\tProject AOI: {project_aoi}\n")
         f.write(f"\tInput Culverts: {input_culverts if input_culverts else 'None'}\n")
-        f.write(f"\tStream Threshold: {stream_threshold}\n")
+        f.write(f"\tStream Threshold (acres): {stream_threshold}\n")
 
 
 ### Initial Tool Validation ###
@@ -61,15 +61,13 @@ project_name = path.basename(project_workspace)
 log_file_path = path.join(project_workspace, f"{project_name}_log.txt")
 project_dem_name = f"{project_name}_DEM"
 project_dem_path = path.join(project_gdb, project_dem_name)
-input_culverts_path = Describe(input_culverts).catalogPath
-output_culverts_name = f"{project_name}_Culverts"
-output_culverts_path = path.join(project_gdb, 'Layers', output_culverts_name)
-culverts_buffer_path = path.join(scratch_gdb, 'Culverts_Buffer')
-culverts_raster_path = path.join(project_gdb, 'Culverts_Raster') #TODO: should this be in project gdb or scratch?
+culverts_buffer_temp = path.join(scratch_gdb, 'Culverts_Buffer')
+culverts_raster_temp = path.join(scratch_gdb, 'Culverts_Raster')
+hydro_dem_temp = path.join(scratch_gdb, 'Hydro_DEM')
+culverts_name = f"{project_name}_Culverts"
+culverts_path = path.join(project_gdb, 'Layers', culverts_name)
 streams_name = f"{project_name}_Streams"
 streams_path = path.join(project_gdb, 'Layers', streams_name)
-hydro_dem_name = 'Hydro_DEM'
-hydro_dem_path = path.join(project_gdb, hydro_dem_name)
 flow_accum_name = 'Flow_Accumulation'
 flow_accum_path = path.join(project_gdb, flow_accum_name)
 flow_dir_name = 'Flow_Direction'
@@ -79,41 +77,21 @@ flow_dir_path = path.join(project_gdb, flow_dir_name)
 if not Exists(project_dem_path):
     AddMsgAndPrint('\nCould not locate the project DEM layer for specified AOI. Exiting...', 2)
     exit()
-project_dem_desc = Describe(project_dem_path)
-dem_cell_size = project_dem_desc.meanCellWidth
-dem_sr = project_dem_desc.spatialReference
-linear_units = dem_sr.linearUnitName
 
 ### ESRI Environment Settings ###
-#TODO: set resampling and pyramids like the other DEM tools?
+dem_desc = Describe(project_dem_path)
+dem_cell_size = dem_desc.meanCellWidth
 env.overwriteOutput = True
+env.resamplingMethod = 'BILINEAR'
+env.pyramid = 'PYRAMIDS -1 BILINEAR DEFAULT 75 NO_SKIP'
+env.parallelProcessingFactor = '75%'
 env.extent = 'MINOF'
 env.cellSize = dem_cell_size
 env.snapRaster = project_dem_path
-env.outputCoordinateSystem = dem_sr
-
-#TODO: Clarify this: Z-factor conversion Lookup table
-# lookup dictionary to convert XY units to area.  Key = XY unit of DEM; Value = conversion factor to sq.meters
-# Assign Z-factor based on XY and Z units of DEM
-# the following represents a matrix of possible z-Factors
-# using different combination of xy and z units
-# ----------------------------------------------------
-#                      Z - Units
-#                       Meter    Foot     Centimeter     Inch
-#          Meter         1	    0.3048	    0.01	    0.0254
-#  XY      Foot        3.28084	  1	      0.0328084	    0.083333
-# Units    Centimeter   100	    30.48	     1	         2.54
-#          Inch        39.3701	  12       0.393701	      1
-# ---------------------------------------------------
-acreConversionDict = {'Meters':4046.8564224,'Meter':4046.8564224,'Foot':43560,'Foot_US':43560,'Feet':43560, 'Centimeter':40470000,'Inch':6273000}
-unitLookUpDict = {'Meter':0,'Meters':0,'Foot':1,'Foot_US':1,'Feet':1,'Centimeter':2,'Centimeters':2,'Inch':3,'Inches':3}
-zFactorList = [[1,0.3048,0.01,0.0254],
-                [3.28084,1,0.0328084,0.083333],
-                [100,30.48,1.0,2.54],
-                [39.3701,12,0.393701,1.0]]
+env.outputCoordinateSystem = dem_desc.spatialReference
 
 try:
-    removeMapLayers(map, [output_culverts_name, streams_name, hydro_dem_name, flow_accum_name, flow_dir_name])
+    removeMapLayers(map, [culverts_name, streams_name, flow_accum_name, flow_dir_name])
     logBasicSettings(log_file_path, project_aoi, input_culverts, stream_threshold)
 
     ### Process Input Culverts ###
@@ -121,75 +99,70 @@ try:
         SetProgressorLabel('Processing input culverts...')
         AddMsgAndPrint('\nProcessing input culverts...', log_file_path=log_file_path)
 
-        if input_culverts_path != output_culverts_path:
+        input_culverts_path = Describe(input_culverts).catalogPath
+        if input_culverts_path != culverts_path:
             SetProgressorLabel('Clipping input culverts to project AOI layer...')
             AddMsgAndPrint('\nClipping input culverts to project AOI layer...', log_file_path=log_file_path)
-            Clip(input_culverts, project_aoi, output_culverts_path)
+            Clip(input_culverts, project_aoi, culverts_path)
         else:
             AddMsgAndPrint('\nExisting project culverts layer used as input...', log_file_path=log_file_path)
 
         # Ensure output culverts layer has at least one feature in AOI
-        if int(GetCount(output_culverts_path).getOutput(0)) > 0:
+        if int(GetCount(culverts_path).getOutput(0)) > 0:
 
             # Buffer the culverts to 1 pixel
             SetProgressorLabel('Buffering culverts by DEM cell size...')
             AddMsgAndPrint('\nBuffering culverts by DEM cell size...', log_file_path=log_file_path)
-            buffer_size = f"{str(dem_cell_size)} Meters"
-            Buffer(output_culverts_path, culverts_buffer_path, buffer_size, 'FULL', 'ROUND', 'NONE')
+            Buffer(culverts_path, culverts_buffer_temp, f"{str(dem_cell_size)} Meters", 'FULL', 'ROUND', 'NONE')
 
             # Dummy field just to execute Zonal Stats on each feature
-            AddField(culverts_buffer_path, 'ZONE', 'TEXT')
-            CalculateField(culverts_buffer_path, 'ZONE', f"!{Describe(culverts_buffer_path).OIDFieldName}!", 'PYTHON3')
+            AddField(culverts_buffer_temp, 'ZONE', 'TEXT')
+            CalculateField(culverts_buffer_temp, 'ZONE', f"!{Describe(culverts_buffer_temp).OIDFieldName}!", 'PYTHON3')
 
             # Get the minimum elevation value for each culvert
             SetProgressorLabel('Finding minimum elevation of culverts...')
             AddMsgAndPrint('\nFinding minimum elevation of culverts...', log_file_path=log_file_path)
-            culverts_min_value = ZonalStatistics(culverts_buffer_path, 'ZONE', project_dem_path, 'MINIMUM', 'NODATA')
-            culverts_min_value.save(culverts_raster_path)
+            culverts_min_value = ZonalStatistics(culverts_buffer_temp, 'ZONE', project_dem_path, 'MINIMUM', 'NODATA')
 
             # Elevation cells that overlap the culverts will get the minimum elevation value
-            mosaic_list = f"{project_dem_path};{culverts_raster_path}"
-            MosaicToNewRaster(mosaic_list, project_gdb, hydro_dem_name, '#', '32_BIT_FLOAT', dem_cell_size, '1', 'LAST')
+            mosaic_list = f"{project_dem_path};{culverts_min_value}"
+            MosaicToNewRaster(mosaic_list, scratch_gdb, 'Hydro_DEM', '#', '32_BIT_FLOAT', dem_cell_size, '1', 'LAST')
 
-            hydro_dem_fill = Fill(hydro_dem_path)
+            hydro_dem_fill = Fill(hydro_dem_temp)
 
     else:
-        SetProgressorLabel('No culverts within project AOI...')
         AddMsgAndPrint('\nNo culverts within project AOI...', log_file_path=log_file_path)
         hydro_dem_fill = Fill(project_dem_path)
 
     ### Create Flow Direction Grid ###
     SetProgressorLabel('Creating Flow Direction...')
-    AddMsgAndPrint('\nCreating Flow Direction...')
+    AddMsgAndPrint('\nCreating Flow Direction...', log_file_path=log_file_path)
     flow_direction = FlowDirection(hydro_dem_fill, 'NORMAL')
     flow_direction.save(flow_dir_path)
 
     ### Create Flow Accumulation Grid ###
     SetProgressorLabel('Creating Flow Accumulation...')
     AddMsgAndPrint('\nCreating Flow Accumulation...', log_file_path=log_file_path)
-    flow_accumulation = FlowAccumulation(flow_dir_path, '', 'INTEGER')
+    flow_accumulation = FlowAccumulation(flow_dir_path, data_type='INTEGER')
     flow_accumulation.save(flow_accum_path)
     # Compute a histogram for the FlowAccumulation layer so that the full range of values are captured for subsequent stream generation
     # This tries to fix a bug of the primary channel not generating for large watersheds with high values in flow accumulation grid
     CalculateStatistics(flow_accum_path)
 
     ### Create Stream Link ###
-    #TODO: Why is this checking for grater than 0, else??
     if stream_threshold > 0:
         # created using pixels that have a flow accumulation greater than the user-specified acre threshold
-        #TODO: Do we need acreConversionDict??
-        acreConvFactor = acreConversionDict.get(linear_units)
-        acreThresholdVal = round((stream_threshold * acreConvFactor)/(dem_cell_size**2))
+        acre_threshold = round((stream_threshold * 4046.8564224)/(dem_cell_size**2))
 
         # Select all cells that are greater than or equal to the acre stream threshold value
-        con_flow_accumulation = Con(flow_accum_path, flow_accum_path, '', f"Value >= {str(acreThresholdVal)}")
+        con_flow_accumulation = Con(flow_accum_path, flow_accum_path, where_clause=f"Value >= {str(acre_threshold)}")
 
         # Create Stream Link Works
         SetProgressorLabel('Creating Stream Link...')
         AddMsgAndPrint('\nCreating Stream Link...', log_file_path=log_file_path)
         stream_link = StreamLink(con_flow_accumulation, flow_dir_path)
 
-    # All values in flowAccum will be used to create stream link
+    # All values in Flow Accumulation will be used to create stream link
     else:
         SetProgressorLabel('Creating Stream Link...')
         AddMsgAndPrint('\nCreating Stream Link...', log_file_path=log_file_path)
@@ -200,13 +173,9 @@ try:
     AddMsgAndPrint('\nCreating Stream Network...', log_file_path=log_file_path)
     StreamToFeature(stream_link, flow_dir_path, streams_path, 'SIMPLIFY')
 
-    # Delete unwanted datasets
-    Delete(hydro_dem_fill)
-    Delete(stream_link)
-
     ### Add Outputs to Map ###
     SetParameterAsText(3, streams_path)
-    SetParameterAsText(4, output_culverts_path)
+    SetParameterAsText(4, culverts_path)
 
     ### Remove Digitized Layer (if present) ###
     for lyr in map.listLayers():
