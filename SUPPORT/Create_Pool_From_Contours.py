912d587bf2cb0d0953093be76ccd66e9fa086146
dylan-harwell <12001003312449@FEDIDCARD.GOV>
2025-06-06 16:06:48 -0700

    WIP: revisions and additions to Pool From Contours, working

--- SUPPORT/Create_Pool_From_Contours.py
+++ SUPPORT/Create_Pool_From_Contours.py
@@ -3,15 +3,16 @@ from os import path
 from sys import argv, exit
 from time import ctime
 
-from arcpy import CheckExtension, CheckOutExtension, Describe, env, Exists, GetInstallInfo, GetParameterAsText, SetParameterAsText, SetProgressorLabel
+from arcpy import CheckExtension, CheckOutExtension, Describe, env, Exists, GetInstallInfo, GetParameterAsText, \
+    SetParameterAsText, SetProgressorLabel
 from arcpy.analysis import Buffer, Clip, Erase
-from arcpy.conversion import FeatureToRaster, RasterToPolygon
+from arcpy.conversion import RasterToPolygon
 from arcpy.da import SearchCursor, UpdateCursor
 from arcpy.ddd import SurfaceVolume
-from arcpy.management import AddField, CalculateField, Compact, CopyFeatures, Delete, Dissolve, GetCount, GetRasterProperties, FeatureToPolygon, \
-    MakeFeatureLayer, SelectLayerByAttribute, SelectLayerByLocation
+from arcpy.management import AddField, CalculateField, Compact, CopyFeatures, Delete, Dissolve, GetCount, \
+    FeatureToPolygon, MakeFeatureLayer, SelectLayerByAttribute, SelectLayerByLocation
 from arcpy.mp import ArcGISProject
-from arcpy.sa import ExtractByMask, Int, Minus, SetNull, Times, ZonalStatistics, ZonalStatisticsAsTable
+from arcpy.sa import ExtractByMask, Int, SetNull, Times, ZonalStatisticsAsTable
 
 from utils import AddMsgAndPrint, emptyScratchGDB, errorMsg, removeMapLayers
 
@@ -55,12 +56,12 @@ project_contours_path = Describe(project_contours).CatalogPath
 if 'EngPro.gdb' in project_contours_path and 'Contour' in project_contours_path:
     project_gdb = project_contours_path[:project_contours_path.find('.gdb')+4]
 else:
-    AddMsgAndPrint('\nThe selected contours layer is not from an Engineering Tools project or is not compatible with this version of the toolbox. Exiting...', 2)
+    AddMsgAndPrint('\nThe selected layer does not appear to be the project Contours or is not compatible with this version of the toolbox. Exiting...', 2)
     exit()
 
-#TODO: Do we also need to validate for only 1 feature? Tool won't work if two lines given
-if not int(GetCount(input_dam).getOutput(0)) > 0:
-    AddMsgAndPrint('\nAt least one dam feature is required. Exiting...', 2)
+### Validate Input Dam ###
+if int(GetCount(input_dam).getOutput(0)) != 1:
+    AddMsgAndPrint('\nA single input line feature is required. Digitize a single line, or if using an existing dams layer, ensure one line is selected. Exiting...', 2)
     exit()
 
 ### Set Paths and Variables ###
@@ -96,16 +97,7 @@ buffer7 = path.join(scratch_gdb, 'Buffer7')
 extent_mask = path.join(scratch_gdb, 'Extent_Mask')
 dams_temp = path.join(scratch_gdb, 'Dams_Temp')
 dams_stats = path.join(scratch_gdb, 'Dams_Stats')
-dem_clip = path.join(scratch_gdb, 'DEM_Clip')
-dem_minus = path.join(scratch_gdb, 'DEM_Minus')
-dem_set_null = path.join(scratch_gdb, 'DEM_SetNull')
-dem_set_null_u = path.join(scratch_gdb, 'DEM_SetNull_u') #TODO: ??
-extent_raster = path.join(scratch_gdb, 'Extent_Raster')
-pool_raster1 = path.join(scratch_gdb, 'Pool_Raster1')
-pool_raster2 = path.join(scratch_gdb, 'Pool_Raster2')
 pool_mask = path.join(scratch_gdb, 'Pool_Mask')
-pool_polygon = path.join(scratch_gdb, 'Pool_Polygon')
-volume_grid = path.join(scratch_gdb, 'Volume_Grid')
 volume = path.join(scratch_gdb, 'Volume')
 temp_pool = path.join(scratch_gdb, 'Temp_Pool')
 
@@ -134,19 +126,6 @@ try:
     AddField(dams_temp, 'LengthFt', 'DOUBLE')
     AddField(dams_temp, 'TopWidth', 'DOUBLE')
     AddField(dams_temp, 'BotWidth', 'DOUBLE')
-
-    # Raster Volume Conversions for acre-feet
-    #NOTE: DEM always in WGS84 UTM, xy meters, elevation feet
-    # if units == 'Meter':
-    convFactor = 0.000810714
-    units = 'Meters'
-
-    # Elevation Conversions
-    # contFactor is for converting contours in feet to the zUnits values
-    # Zfactor is for converting the zUnits to values to feet
-    # elif zUnits == 'Feet':
-    contFactor = 1              #feet to feet
-    Zfactor = 1                 #feet to feet
 
     CalculateField(dams_temp, 'ID', '!OBJECTID!', 'PYTHON3')
     CalculateField(dams_temp, 'LengthFt', "!shape!.getLength('PLANAR', 'FeetInt')", 'PYTHON3')
@@ -185,24 +164,17 @@ try:
     SelectLayerByLocation(contour_lyr, 'INTERSECT', buffer6, '', 'NEW_SELECTION')
     MakeFeatureLayer(contour_lyr, contour_lyr2)
 
-    #NOTE: What is CONTOUR + D ???
-    # with SearchCursor(contour_lyr2, '', '', 'CONTOUR', 'CONTOUR' + ' D') as cursor:
-    #     row = cursor.next()
-    #     hi_contour = row.CONTOUR
     hi_contour = 0
     with SearchCursor(contour_lyr2, ['Contour']) as cursor:
         for row in cursor:
             if row[0] > hi_contour: hi_contour = row[0]
-
-    # # Assign the pool elevation in the actual units of the DEM
-    # poolElev = (hi_contour * contFactor)
 
     AddMsgAndPrint(f"\nHighest closed contour is {hi_contour} ft...")
 
     # Output Datasets
     output_pool_name = f"{project_name}_Pool_{str(hi_contour).replace('.','_dot_')}"
     output_pool_path = path.join(project_fd, output_pool_name)
-    output_dams_name = f"{output_pool_name}_Dams"
+    output_dams_name = f"{output_pool_name}_Dam"
     output_dams_path = path.join(project_fd, output_dams_name)
     storage_table_temp = path.join(project_workspace, f"{output_pool_name}_StorageCSV.txt")
 
@@ -213,30 +185,6 @@ try:
 
     # Dissolve and populate with plane elevation for raster processing
     Dissolve(extent_mask, pool_mask)
-    #TODO: Why add field here and not to the output?
-    # AddField(pool_mask, 'DemElev', 'DOUBLE')
-    # CalculateField(pool_mask, 'DemElev', hi_contour, 'PYTHON3')
-
-    #TODO: What if multiple dams in different areas, different 'hi_contours'? 
-
-    #TODO: Why convert to raster? ExtractByMask can use a polygon feature
-    # Convert to raster, clip DEM and create Pool polygon
-    # MakeFeatureLayer(pool_mask, 'pool_mask')
-    # FeatureToRaster('pool_mask', 'DemElev', extent_raster, dem_cell_size)
-    # outMask = ExtractByMask(project_dem, extent_raster)
-    # outMask.save(dem_clip)
-
-    #TODO: Is this necessary? Contours are created from DEM, so digitized dam (elevation specified on contours) should be within DEM - not a number input
-    # # User specified max elevation value must be within min-max range of elevation values in clipped dem
-    # demClipMaxElev = round(((float(GetRasterProperties(dem_clip, 'MAXIMUM').getOutput(0)))* Zfactor),1)
-    # demClipMinElev = round(((float(GetRasterProperties(dem_clip, 'MINIMUM').getOutput(0)))* Zfactor),1)
-    # if not demClipMinElev < hi_contour <= demClipMaxElev:
-    #     AddMsgAndPrint('\n\tThe Pool Elevation Specified is not within the range', 2)
-    #     AddMsgAndPrint('\tof the corresponding area of your input DEM.', 2)
-    #     AddMsgAndPrint('\tPlease specify a value between ' + str(demClipMinElev) + ' and ' + str(demClipMaxElev) + '. Exiting...', 2)
-    #     exit()
-
-    #NOTE: New procedure starts here - follows same as Create Pool At Specified Elevation tool
     temp_dem = ExtractByMask(project_dem, pool_mask)
 
     # Convert DEM elevation units to meters for processing
@@ -268,7 +216,7 @@ try:
     AddField(output_pool_path, 'CUBIC_FEET', 'DOUBLE')
     AddField(output_pool_path, 'CUBIC_METERS', 'DOUBLE')
 
-    # Open surface volume text file - last line should represent the last pool
+    # Open surface volume text file
     with open(storage_table_temp) as file:
         lines = file.readlines()
 
@@ -304,103 +252,27 @@ try:
     if Exists(temp_dem):
         Delete(temp_dem)
 
-    #NOTE: Original procedure below ----------------------------------------
-    # outMinus = Minus(extent_raster, dem_clip)
-    # outMinus.save(dem_minus)
-
-    # outNull = SetNull(dem_minus, dem_minus, 'Value <= 0')
-    # outNull.save(dem_set_null)
-
-    # # DEMsn is now a depth raster expressed in the original vertical units. Convert it to depths units that match the xy units.
-    # # if units == 'Meters':
-    # #     elif zUnits == 'Feet':
-    # factor = 0.3048
-
-    # outConv = Times(dem_set_null, factor)
-    # outConv.save(dem_set_null_u)
-
-    # outTimes = Times(dem_set_null_u, 0)
-    # outTimes.save(pool_raster1)
-
-    # outInt = Int(pool_raster1)
-    # outInt.save(pool_raster2)
-
-    # RasterToPolygon(pool_raster2, pool_polygon, 'NO_SIMPLIFY', 'VALUE')
-
-    # # Dissolve results and add fields
-    # Dissolve(pool_polygon, output_pool_path)
-    # AddField(output_pool_path, 'ID', 'LONG')
-    # AddField(output_pool_path, 'DemElev', 'DOUBLE')
-    # AddField(output_pool_path, 'PoolElev', 'DOUBLE')
-    # AddField(output_pool_path, 'PoolAcres', 'DOUBLE')
-    # AddField(output_pool_path, 'RasterVol', 'DOUBLE')
-    # AddField(output_pool_path, 'AcreFt', 'DOUBLE')
-
-    # Calculate Area, Raster Elevation and Pool Elevation
-    # with UpdateCursor(output_pool_path) as cursor:
-    #     for row in cursor:
-    #         row.ID = row.OBJECTID
-    #         row.DemElev = str(poolElev)
-    #         row.PoolElev = str(hi_contour)
-    #         if units == 'Meters':
-    #             row.PoolAcres = str(round(row.Shape_Area / 4046.86,1))
-    #         else:
-    #             row.PoolAcres = str(round(row.Shape_Area / 43560,1))
-    #         cursor.updateRow(row)
-
-    #         AddMsgAndPrint('\nCalculating pool area and volume')
-
-    #         volTimes = Times(dem_set_null_u, dem_cell_area)
-    #         volTimes.save(volume_grid)
-
-    #         outZonal = ZonalStatistics(output_pool_path, 'ID', volume_grid, 'SUM')
-    #         outZonal.save(volume)
-
-    #         # Get results and populate remaining fields
-    #         row.RasterVol = str(round(float(GetRasterProperties(volume, 'MAXIMUM').getOutput(0)),1))
-    #         row.AcreFt = str(round(float(row.RasterVol * convFactor),1))
-    #         AddMsgAndPrint('\n\t\tPool Elevation: ' + str(row.PoolElev) + ' Feet')
-    #         AddMsgAndPrint('\t\tPool Area: ' + str(row.PoolAcres) + ' Acres')
-    #         AddMsgAndPrint('\t\tPool volume: ' + str(row.RasterVol) + ' Cubic ' + str(units))
-    #         AddMsgAndPrint('\t\tPool volume: ' + str(row.AcreFt) + ' Acre Feet')
-    #         cursor.updateRow(row)
-    #-----------------------------------------------------------------------
-
     # Retrieve attributes for dam and populate fields
     Buffer(output_dams_path, buffer7, '3 Meters', 'RIGHT', 'ROUND', 'LIST', 'ID')
-    #TODO: Why adding field? Its not calculated or updated below
-    # AddField(buffer7, 'ELEV', 'DOUBLE')
     ZonalStatisticsAsTable(buffer7, 'ID', project_dem, dams_stats, 'NODATA', 'ALL')
 
     with SearchCursor(dams_stats, ['MIN','MAX','MEAN']) as s_cursor:
-        #TODO: Should only have one row unless we allow multiple input dams
         for s_row in s_cursor:
-            #TODO: Dont think we need to query for ID unless we allow multiple input dams
-            # id = s_row.ID
-
             minElev = s_row[0]
             maxElev = s_row[1]
             meanElev = s_row[2]
-
-            #TODO: Dam Stats table values are in feet, no need to convert with zfactor
-            maxFt = round(float(maxElev * Zfactor),1)
-            minFt = round(float(minElev * Zfactor),1)
-            meanFt = round(float(meanElev * Zfactor),1)
+            maxFt = round(float(maxElev),1)
+            minFt = round(float(minElev),1)
+            meanFt = round(float(meanElev),1)
 
             AddMsgAndPrint('\n\tCalculating properties of embankment ' + str(id))
             AddMsgAndPrint('\n\t\tMax Elevation: ' + str(maxFt) + ' Feet')
             AddMsgAndPrint('\t\tMin Elevation: ' + str(minFt) + ' Feet')
             AddMsgAndPrint('\t\tMean Elevation: ' + str(meanFt) + ' Feet')
 
-            # query = f"ID = {id}"
-            # SelectLayerByAttribute(dams_lyr, 'NEW_SELECTION', query)
-
-            damHeight = int(maxFt - minFt)
+            damHeight = int(maxFt-minFt)
             AddMsgAndPrint('\t\tMax Height: ' + str(damHeight) + ' Feet')
 
-            #TODO: no need to query unless we allow multiple dams
-            # whereclause = f"ID = {id}"
-            # with UpdateCursor(output_dams_path, whereclause) as u_cursor:
             with UpdateCursor(output_dams_path, ['LengthFt','MaxElev','MinElev','MeanElev','TopWidth','BotWidth']) as u_cursor:
                 for u_row in u_cursor:
                     damLength = u_row[0]
@@ -439,6 +311,11 @@ try:
     # ### Add Output to Map ###
     SetParameterAsText(2, output_pool_path)
     SetParameterAsText(3, output_dams_path)
+
+    ### Remove Digitized Layer (if present) ###
+    for lyr in map.listLayers():
+        if 'Create Pool from Contours' in lyr.name:
+            map.removeLayer(lyr)
 
     ### Compact Project GDB ###
     try:
